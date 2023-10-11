fc7d243e5357270ca40976b4a5543bf8b8e4d306
memoryBangwool <jihyuneee01@gmail.com>
2023-10-11 23:28:49 +0900

    week1 필수과제 기능 구현

--- app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
@@ -1,48 +1,52 @@
 package org.sopt.dosopttemplate
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import  org.sopt.dosopttemplate.databinding.ActivitySignupBinding
+import org.sopt.dosopttemplate.util.ToastMaker.makeToast
 
-class SingUpActivity :AppCompatActivity() {
+class SingUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btSingUp.setOnClickListener() {
-            signUpAttempt()
+        clickSignUpBtn()
+    }
+
+    private fun clickSignUpBtn() {
+        binding.btSignUp.setOnClickListener() {
+            if (isConditionSatisfied()) {
+                makeToast(this, "회원가입 완료!")
+                sendDataToLoginActivity()
+            } else
+                makeToast(this, "입력값이 조건에 맞지 않습니다.")
         }
     }
-    private fun signUpAttempt(){
-        if(conditionSatisfied()){
-            makeToast("회원가입 완료!")
-            sendDataToLoginActivity()
+
+    private fun sendDataToLoginActivity() {
+        with(intent) {
+            putExtra("id", binding.etId.text.toString())
+            putExtra("pw", binding.etPw.text.toString())
+            putExtra("nickName", binding.etNickName.text.toString())
+            putExtra("mbti", binding.etMbti.text.toString())
+            putExtra("intro", binding.etIntro.text.toString())
         }
-        else
-            makeToast("입력값이 조건에 맞지 않습니다.")
-    }
-    private fun sendDataToLoginActivity(){
-        intent.putExtra("id", binding.etId.text.toString())
-            .putExtra("pw", binding.etPw.text.toString())
-            .putExtra("nickName", binding.etNickName.text.toString())
-            .putExtra("mbti", binding.etMbti.text.toString())
-            .putExtra("intro", binding.etIntro.text.toString())
         setResult(RESULT_OK, intent)
         finish()
     }
-    private fun conditionSatisfied():Boolean{
+
+    private fun isConditionSatisfied(): Boolean {
         return ((binding.etId.length() in 6..10)
-                &&(binding.etPw.length() in 8..12)
-                &&(binding.etNickName.length()>=1 && !binding.etNickName.equals(" "))
-                &&(binding.etMbti.length()==4 && isAlphabet(binding.etMbti.text.toString()))
-                &&(binding.etIntro.length()!=0))
+                && (binding.etPw.length() in 8..12)
+                && (binding.etNickName.length() >= 1 && !binding.etNickName.equals(" "))
+                && (binding.etMbti.length() == 4 && isAlphabet(binding.etMbti.text.toString()))
+                && (binding.etIntro.length() != 0))
     }
-    private fun isAlphabet(string:String):Boolean{
+
+    private fun isAlphabet(string: String): Boolean {
         var lengthOfAlphabet = string.filter { it in 'A'..'Z' || it in 'a'..'z' }.length
         return (lengthOfAlphabet == string.length)
     }
-    private fun makeToast(msg:String) =  Toast.makeText(this.applicationContext, msg, Toast.LENGTH_SHORT).show()
 }
 
--- app/src/main/java/org/sopt/dosopttemplate/loginActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/loginActivity.kt
@@ -6,56 +6,59 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import org.sopt.dosopttemplate.databinding.ActivityMainBinding
-
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var id:String
-    private lateinit var pw:String
-    private lateinit var mbti:String
-    private lateinit var nickName:String
-    private lateinit var intro:String
-    lateinit var resultLauncher:ActivityResultLauncher<Intent>
+    private lateinit var id: String
+    private lateinit var pw: String
+    private lateinit var mbti: String
+    private lateinit var nickName: String
+    private lateinit var intro: String
+    lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btLogin.setOnClickListener() {
-            loginAttempt()
-        }
+
         getSignUpData()
-        binding.btSingUp.setOnClickListener(){
-            signUp()
+        clickLoginBtn()
+        clickSignUpBtn()
+    }
+
+    private fun clickLoginBtn() {
+        binding.btLogin.setOnClickListener() {
+            if (isLoginAuthorized()) {
+                makeToast("로그인 성공!")
+                goToMainPage()
+            } else
+                makeToast("ID 또는 비밀번호가 일치하지 않습니다.")
         }
     }
-    private fun loginAttempt(){
-        if(loginAuthorized()) {
-            makeToast("로그인 성공!")
-            goToMainPage()
-        }
-        else
-            makeToast("ID 또는 비밀번호가 일치하지 않습니다.")
-    }
-    private fun goToMainPage(){
+
+    private fun goToMainPage() {
         val intent = Intent(this, MainPageActivity::class.java)
-        intent.putExtra("id", id)
-        intent.putExtra("mbti", mbti)
-        intent.putExtra("nickName", nickName)
-        intent.putExtra("intro", intro)
+        with(intent) {
+            putExtra("id", id)
+            putExtra("mbti", mbti)
+            putExtra("nickName", nickName)
+            putExtra("intro", intro)
+        }
         startActivity(intent)
     }
-    private fun loginAuthorized():Boolean{
-        var enteredID = binding.etId.text.toString()
-        var enteredPW = binding.etPassword.text.toString()
+
+    private fun isLoginAuthorized(): Boolean {
+        val enteredID = binding.etId.text.toString()
+        val enteredPW = binding.etPassword.text.toString()
         return (enteredID == id && enteredPW == pw)
     }
-    private fun getSignUpData(){
+
+    private fun getSignUpData() {
         resultLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()) { result ->
-            if(result.resultCode == RESULT_OK) {
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
                 id = result.data?.getStringExtra("id") ?: ""
                 pw = result.data?.getStringExtra("pw") ?: ""
                 nickName = result.data?.getStringExtra("nickName") ?: ""
@@ -64,11 +67,15 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
-    private fun makeToast(msg:String){
+
+    private fun makeToast(msg: String) {
         Toast.makeText(this.applicationContext, msg, Toast.LENGTH_SHORT).show()
     }
-    private fun signUp(){
-        val intent = Intent(this, SingUpActivity::class.java)
-        resultLauncher.launch(intent)
+
+    private fun clickSignUpBtn() {
+        binding.btSignUp.setOnClickListener() {
+            val intent = Intent(this, SingUpActivity::class.java)
+            resultLauncher.launch(intent)
+        }
     }
 }
