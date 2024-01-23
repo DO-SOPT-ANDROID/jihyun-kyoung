10710b9ee6f24dc7ae288356501750ca3c7486c7
jihyun0v0 <jihyuneee01@gmail.com>
2024-01-23 14:55:28 +0900

    [fix]: develop 충돌 해결

--- app/src/main/java/org/sopt/dosopttemplate/util/IntentExt.kt
+++ app/src/main/java/org/sopt/dosopttemplate/util/IntentExt.kt
@@ -1,20 +1,17 @@
 package org.sopt.dosopttemplate.util
 
 import android.app.Activity
-import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.content.Intent
 import android.os.Build
 import android.util.Log
-import org.sopt.dosopttemplate.data.Profile
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
+import org.sopt.dosopttemplate.data.Profile
 
 fun <T> Intent.getParcelable(name: String, clazz: Class<T>): T? {
-    return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         getParcelableExtra(name, clazz)
-    }
-    else
+    } else
         getParcelableExtra(name)
 }
 
@@ -24,8 +21,9 @@ fun logProfile(profile: Profile) {
     Log.v("mbti", profile.MBTI.toString())
     Log.v("music title", profile.getMusic())
     Log.v("intro", profile.intro.toString())
+}
 
-fun hideKeyboard(activity:Activity) {
+fun hideKeyboard(activity: Activity) {
     val imm: InputMethodManager =
         activity.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(activity.currentFocus?.windowToken, 0)
